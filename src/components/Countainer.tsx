e923d4fcc00a4c0c1e7b10b6b315fcf09670b057
Mani <maniataai@gmail.com>
2025-03-28 13:18:10 +0330

    make base file and add ui kit

---
+++ src/components/Countainer.tsx
@@ -0,0 +1,11 @@
+import React, { ReactNode } from 'react';
+
+interface ContainerProps {
+  children: ReactNode;
+}
+
+export default function Container({ children }: ContainerProps) {
+  return (
+    <div>{children}</div>
+  );
+}
